98d1e4d60359ed71872048f5429c5046e36beae3
何知曲成溪 <1242079396@qq.com>
2023-03-17 18:56:14 +0800

    二叉树中序遍历

---
+++ easy/94.二叉树的中序遍历.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  const res = [];
+  const fn = (root) => {
+    if(!root) {
+      return
+    }
+    fn(root.left);
+    res.push(root.val);
+    fn(root.right)
+  }
+  fn(root);
+  return res;
+};
+// @lc code=end
+
